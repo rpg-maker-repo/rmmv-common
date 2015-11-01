19df589d077984c178925bc70228cde9cdcce927
Michael Main <deusprogrammer@gmail.com>
2015-10-31 23:14:18 -0500

    Initial commit

---
+++ src/com/trinary/rpgmaker/ro/TagRO.java
@@ -0,0 +1,23 @@
+package com.trinary.rpgmaker.ro;
+
+import com.trinary.ro.RepresentationObject;
+
+public class TagRO extends RepresentationObject {
+	String value;
+
+	/**
+	 * @return the value
+	 */
+	public String getValue() {
+		return value;
+	}
+
+	/**
+	 * @param value the value to set
+	 */
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+	
+}
